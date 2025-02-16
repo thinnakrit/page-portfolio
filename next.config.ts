87537350d811b3d2fa23de54b3cb4b217ceb28e1
JAY <57744@tmbbank.com>
2025-02-16 23:16:29 +0700

    Update gp-page

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   assetPrefix: isProd ? "/page-portfolio/" : "",
+  basePath: isProd ? "/page-portfolio" : "",
+  output: "export",
 };
 
 export default nextConfig;
