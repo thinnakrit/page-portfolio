a3e1801cc45d1cac1109f244bd03d44a22b0c486
JAY <57744@tmbbank.com>
2025-02-16 23:36:50 +0700

    Update gp-page

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? "/" : "",
-  basePath: isProd ? "/" : "",
   output: "export",
 };
 
